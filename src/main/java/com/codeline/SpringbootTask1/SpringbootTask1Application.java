9da7b6f705c621b0f12df83d8b754c95ed5999c6
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-07 14:25:23 +0400

    first commit

---
+++ src/main/java/com/codeline/SpringbootTask1/SpringbootTask1Application.java
@@ -0,0 +1,13 @@
+package com.codeline.SpringbootTask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTask1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeline/SpringbootTask1/SpringbootTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeline.SpringbootTask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTask1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
